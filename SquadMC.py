1c9b6d0aa02d9041125e3e4788f0f7271db08252
Grinch27 <Grinch27@outlook.com>
2023-12-03 13:57:42 +0800

    use class define

--- SquadMC.py
+++ SquadMC.py
@@ -13,91 +13,101 @@ class Interpolator:
                 return self.data[i][1] + slope * (range_value - self.data[i][0])
         return "N/A"
 
+    @staticmethod
+    def format_angle(angle):
+        return angle if isinstance(angle, str) else format(angle, ".2f")
 
-def format_angle(angle):
-    return angle if isinstance(angle, str) else format(angle, ".2f")
+
+class Mortar(Interpolator):
+    def __init__(self):
+        super().__init__(
+            [
+                (50, 1579),
+                (100, 1558),
+                (150, 1538),
+                (200, 1517),
+                (250, 1496),
+                (300, 1475),
+                (350, 1453),
+                (400, 1431),
+                (450, 1409),
+                (500, 1387),
+                (550, 1364),
+                (600, 1341),
+                (650, 1317),
+                (700, 1292),
+                (750, 1267),
+                (800, 1240),
+                (850, 1212),
+                (900, 1183),
+                (950, 1152),
+                (1000, 1118),
+                (1050, 1081),
+                (1100, 1039),
+                (1150, 988),
+                (1200, 918),
+                (1250, 800),
+            ]
+        )
 
 
-def main():
-    distance = float(input("des："))
+class Tank(Interpolator):
+    def __init__(self):
+        super().__init__(
+            [
+                (925, 45),
+                (900, 50),
+                (875, 55),
+                (800, 60),
+                (700, 65),
+                (600, 70),
+                (500, 73.5),
+                (400, 77),
+                (300, 80.5),
+                (200, 83.5),
+                (150, 85),
+                (0, 90),
+            ]
+        )
 
-    data_mortar = [
-        (50, 1579),
-        (100, 1558),
-        (150, 1538),
-        (200, 1517),
-        (250, 1496),
-        (300, 1475),
-        (350, 1453),
-        (400, 1431),
-        (450, 1409),
-        (500, 1387),
-        (550, 1364),
-        (600, 1341),
-        (650, 1317),
-        (700, 1292),
-        (750, 1267),
-        (800, 1240),
-        (850, 1212),
-        (900, 1183),
-        (950, 1152),
-        (1000, 1118),
-        (1050, 1081),
-        (1100, 1039),
-        (1150, 988),
-        (1200, 918),
-        (1250, 800),
-    ]
-    data_tank = [
-        (925, 45),
-        (900, 50),
-        (875, 55),
-        (800, 60),
-        (700, 65),
-        (600, 70),
-        (500, 73.5),
-        (400, 77),
-        (300, 80.5),
-        (200, 83.5),
-        (150, 85),
-        (0, 90),
-    ]
-    data_rocket = [
-        (50, 0),
-        (250, 2.5),
-        (500, 5),
-        (700, 7.5),
-        (800, 8.8),
-        (900, 10),
-        (1000, 12),
-        (1100, 13.5),
-        (1200, 15.5),
-        (1300, 16.3),
-        (1400, 18),
-        (1500, 20),
-        (1600, 22.5),
-        (1700, 25),
-        (1800, 27.5),
-        (1900, 29.8),
-        (2000, 32),
-    ]
 
-    interpolator_mortar = Interpolator(data_mortar)
-    interpolator_tank = Interpolator(data_tank)
-    interpolator_rocket = Interpolator(data_rocket)
+class Rocket(Interpolator):
+    def __init__(self):
+        super().__init__(
+            [
+                (50, 0),
+                (250, 2.5),
+                (500, 5),
+                (700, 7.5),
+                (800, 8.8),
+                (900, 10),
+                (1000, 12),
+                (1100, 13.5),
+                (1200, 15.5),
+                (1300, 16.3),
+                (1400, 18),
+                (1500, 20),
+                (1600, 22.5),
+                (1700, 25),
+                (1800, 27.5),
+                (1900, 29.8),
+                (2000, 32),
+            ]
+        )
 
-    mortar_angle = interpolator_mortar.interpolate_angle(distance)
-    tank_angle = interpolator_tank.interpolate_angle(distance)
-    rocket_angle = interpolator_rocket.interpolate_angle(distance)
 
-    mortar_angle_str = format_angle(mortar_angle)
-    tank_angle_str = format_angle(tank_angle)
-    rocket_angle_str = format_angle(rocket_angle)
+def main(distance):
+    mortar = Mortar()
+    tank = Tank()
+    rocket = Rocket()
 
-    print(
-        f"mortar:{mortar_angle_str}, tank:{tank_angle_str}, rocket:{rocket_angle_str}"
-    )
+    mortar_angle = mortar.format_angle(mortar.interpolate_angle(distance))
+    tank_angle = tank.format_angle(tank.interpolate_angle(distance))
+    rocket_angle = rocket.format_angle(rocket.interpolate_angle(distance))
+
+    print(f"mortar:{mortar_angle}, tank:{tank_angle}, rocket:{rocket_angle}")
 
 
 if __name__ == "__main__":
-    main()
+    distance = float(input("des："))
+    main(distance)
